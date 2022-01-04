2bca217a248db949431d850ceba847744fdae0ad
Tom Hunt <>
2022-01-04 22:46:39 +0000

    print function and matrix lib

--- sketches/sinesAndCircles.js
+++ sketches/sinesAndCircles.js
@@ -7,139 +7,134 @@ const WIDTH = 150;
 const HEIGHT = 150;
 const SCALEMULT = 2;
 
+let _matrix;
+
 let controls;
 let snapshots;
 
+let _lines;
+let _circle;
+let _border;
+
 function setup() {
+	pixelDensity(1);
 	createCanvas(WIDTH * SCALEMULT, HEIGHT * SCALEMULT);
-	// angleMode(DEGREES); 
+	noLoop();
+
+	_matrix = new Matrix( drawingContext );
 	
 	controls = new Controls();
 	snapshots = new Snapshots(controls);
-  
-	controls.addSlider('numlines', 		1, 100, 20, 1 );
-	controls.addSlider('stretch', 		1, 2, 1, 0 );
+
+	_lines = new LinesComp( controls );
+	_border = new Border( controls );
+	_circle = new Circle(controls);
+
 	controls.addSlider('offset', 		0, 1, 0, 0 );
 	controls.addSlider('power',	 		1, 8, 1, 1 );
 	controls.addSlider('cycles', 		2, 8, 2, 2 );
 	controls.addSlider('squeeze', 		0, 8, 2, 0 );
-	
-	controls.addSlider('radius', 		0, 2, 0.25, 0 );
-	controls.addSlider('circleX', 		0, 1, 0.5, 0 );
-	controls.addSlider('circleY', 		0, 1, 0.5, 0 );
-	controls.addSlider('circleStrk', 	0, 10, 0, 1 );
-
 	controls.addSlider('lengthOffsetX', 0, 2, 0, 0 );
 	controls.addSlider('lengthOffsetY', 0, 1, 0, 0 );
 	controls.addSlider('lengthScale', 	0, 1, 1, 0 );
 	controls.addSlider('lengthFreq', 	0, 4, 1, 0.5 );
 
-	controls.addSlider('rotate',	 	0, 360, 0, 90 );
-	controls.addSlider('outline', 		0, 10, 0, 1 );
-	controls.addSlider('border', 		1, 30, 1, 1 );
+	controls.addSlider('rotate',	 	0, 360, 0, 5 );	
 
 	snapshots.getLatest();
+
+	let printButton = createButton( 'PRINT' );
+	printButton.mousePressed( print );
 }
 
-class Line {
-  constructor( position, parent=null ) {
-	this.position = position;
-	this.parent = parent;
-	this.offset = position;
-	this.length = 1;
-  };
-  
-  render( scaleX, scaleY ) {
+function print() {
+	let printCommands = {}
 
-	let x = this.offset;
-	let y = this.length;	
+	// border
+	let borderPrint = {
+		thickness : {
+			x : ((_border.borderThickness.x * WIDTH) / 10).toFixed(1),
+			y : ((_border.borderThickness.y * HEIGHT) / 10).toFixed(1),
+		},
+		outline : _border.outlineThickness
+	};
+	printCommands['border'] = borderPrint;
 
-	x *= scaleX;
-	y *= scaleY;
-
-	y = max( min( y, height ), 0 );
-	line( x , 0, x, y );
-	
-  };
-}
-
-
-class LinesComp {
-  constructor( numlines ) {
-	this.numlines = numlines;
-	this.lines = [];
-	
-	this.root = new Line( 0 );
-	
-	this.lines.push( new Line( 0, this.root ) );    
-	for( let i=1; i<=numlines; i++ ) {
-	  let position = i / numlines;
-	  let line = new Line( position, this.lines[i-1] )
-	  this.lines.push( line );
+	// circle
+	let circlePrint = {
+		radius : ((_circle.radius * WIDTH) / 10 ).toFixed(1),
+		position : {
+			x : ((_circle.position.x * WIDTH) / 10 ),
+			y : ((_circle.position.y * HEIGHT) / 10 )
+		}
 	}
-  };
-  
-  process( func, param ) {
-	for( const line of this.lines ) {
-	  let o = func( line.offset );
-	  line[param] = o;
+	circlePrint.position = _matrix.applyToPoint( circlePrint.position.x, circlePrint.position.y );
+	printCommands['circle'] = circlePrint;
+
+	// _lines
+	let linesPrint = [];
+	const stretch = controls.getValue('stretch');
+	for( const line of _lines.lines ) {
+		let lineInfo = {
+			x : ((line.offset * WIDTH) / 10),
+			y : ((line.length * HEIGHT) / 10)
+		}
+		console.log(lineInfo);
+		lineInfo = _matrix.applyToPoint( lineInfo.x, lineInfo.y );
+		console.log(lineInfo);
+		linesPrint.push(lineInfo);
 	}
-  };
-  
-  render() {
-	for( const line of this.lines ) {
-	  line.render( width * controls.getValue('stretch'), height );
-	}
-  };
-  
+	printCommands['lines'] = linesPrint;
+
+	console.log(printCommands);
 }
 
 
 function draw() {
-  
-  // clear();
 
-  controls.updateDisplay();
-  snapshots.storeLatest();
+	clear();
 
-  const NUMLINES = controls.getValue('numlines');
-  const MULT = 1 / NUMLINES;
-  const OFFSET = (width / NUMLINES) / 2;
-  
-  lines = new LinesComp( NUMLINES );
-  
+	_matrix.reset();
+
+	controls.updateDisplay();
+	snapshots.storeLatest();
+
+	_lines.init();
+	_circle.init();
+	_border.init();
+
 
   // test
-  lines.process(
-	i => i
+  _lines.process(
+  	i => i
   );
   
   // sine compressing in middle
-  lines.process(
-	(i) => {
-		let v = i;
-		v += controls.getValue('offset');
-		v *= controls.getValue('cycles');
+  _lines.process(
+  	(i) => {
+  		let v = i;
+  		v += controls.getValue('offset');
+  		v *= controls.getValue('cycles');
 		// v = pow( v, controls.getValue('power') );
 
 		v = sin( PI * v  );
 		
-		return i + v * ((controls.getValue('squeeze')/NUMLINES))
+		return i + v * ((controls.getValue('squeeze') / _lines.numlines ))
 	},
-	'offset'
-  );
+		'offset'
+	);
 
   // cut height with sine
-  lines.process(
-	(i) => {
-		let v = i;
-		v += controls.getValue( 'lengthOffsetX' );
-		v *= controls.getValue( 'lengthFreq' );
+  _lines.process(
+  	(i) => {
+  		let v = i;
+  		v += controls.getValue( 'lengthOffsetX' );
+  		v *= controls.getValue( 'lengthFreq' );
 
-		v = controls.getValue( 'lengthScale' ) * sin( PI * v ) + controls.getValue( 'lengthOffsetY' );;
-		return v;
-	},
-	'length'
+  		v = controls.getValue( 'lengthScale' ) * sin( PI * v ) + controls.getValue( 'lengthOffsetY' );;
+  		return v;
+  	},
+  	'length'
   );
   
 
@@ -149,46 +144,30 @@ function draw() {
   strokeWeight(1);
 
 
-  push();
-  translate(width/2, height/2);
-  rotate(radians(-controls.getValue('rotate')));
-  // scale( 1-controls.getValue('border') );
-  translate( -width/2, -height/2 );
+  // push();
 
-  push();
-  translate( -((controls.getValue('stretch') - 1 ) / 2) * width, 0, 0 );
-  lines.render();
-  pop();
+  // console.log(_matrix);
+
+  // _matrix.scale(2,2);
+
+  _matrix.translate(width/2, height/2);
+  _matrix.rotate(radians(-controls.getValue('rotate')));
+  _matrix.translate( -width/2, -height/2 );
+
+  // console.log(_matrix);
+
+  // push();
+  _matrix.translate( -((controls.getValue('stretch') - 1 ) / 2) * width, 0, 0 );
+  _lines.render();
+  _matrix.translate( ((controls.getValue('stretch') - 1 ) / 2) * width, 0, 0 );
+  // pop();
   
-  strokeWeight( controls.getValue('circleStrk') );
-  circle( controls.getValue('circleX') * width, controls.getValue('circleY') * height, controls.getValue('radius') * width * 2 );
+  _circle.render();
 
-  // noFill();
-  strokeWeight( controls.getValue('outline') );
-  // rect( 0, 0, width, height );
-
-  const o = (controls.getValue( 'outline' ) * SCALEMULT ) / 2;
-  const bx = ( 1 / controls.getValue( 'border' ) ) * width;
-  const by = ( 1 / controls.getValue( 'border' ) ) * height;
-  
-
-  beginShape();
-  vertex( 0 - o, 0 - o );
-  vertex( width + o, 0 - o );
-  vertex( width + o, height + o );
-  vertex( 0 - o, height + o );
-  
-  beginContour()
-  vertex( bx, height - by );
-  vertex( width - bx, height - by );
-  vertex( width - bx, by );
-  vertex( bx, by );
-  endContour();
-
-  endShape(CLOSE);
+  _border.render();
 
 
-  pop();
+  // pop();
 
 
 }
