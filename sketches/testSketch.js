b116271dd4f762481bc496e9ec823eec7b3ef062
Tom Hunt <>
2022-01-13 17:16:51 +0000

    snapshots buttons

--- sketches/testSketch.js
+++ sketches/testSketch.js
@@ -2,7 +2,11 @@ import Canvas from '../lib/Canvas.js';
 import Line from '../lib/Line.js';
 import Circle from '../lib/Circle.js';
 // import Point from '../lib/Point.js';
+import Controls from '../lib/Controls.js';
+import Snapshots from '../lib/Snapshots.js';
 
+
+let controls;
 
 let c;
 
@@ -14,6 +18,15 @@ window.setup = function() {
 	);
 
 	c = new Circle( 'circle1' );
+
+	controls = Controls.addGroup( 'sketch' );
+	controls.addControl( 'noise', 0, 1, 0, 0.01 );
+	controls.addControl( 'freq', 0, 0.1, 0, 0.001 );
+	controls.addControl( 'time', 0, 10, 0, 0.01 );
+
+	Snapshots.init( 'Test1' );
+	// Snapshots.loadSnapshots();
+
 }
 
 
@@ -37,12 +50,17 @@ window.draw = function() {
 	
 	c.create(
 		c.controls.getValue('radius'),
-		new p5.Vector( 0, 0 ),
+		new p5.Vector( 0, 0, 0 ),
 		c.controls.getValue('resolution'),
 		c.controls.getValue('start'),
-		c.controls.getValue('end'),
+		c.controls.getValue('end')
 	);
-
+	
+	c.noise( 
+		controls.getValue('noise'),
+		controls.getValue('freq'),
+		controls.getValue('time')
+	);
 	c.rotate( c.controls.getValue('rotate') );
 
 	c.render();
