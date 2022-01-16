b744b3b1a990c150bfe21b6ce1ac8d3629ba234a
Tom Hunt <>
2022-01-16 12:47:51 +0000

    grid

--- sketches/testSketch2.js
+++ sketches/testSketch2.js
@@ -1,13 +1,16 @@
 import Canvas from '../lib/Canvas.js';
 import Line from '../lib/Line.js';
 import Circle from '../lib/Circle.js';
+import Grid from '../lib/Grid.js';
 
 import Controls from '../lib/Controls.js';
 import Snapshots from '../lib/Snapshots.js';
 
 import * as TransMatrix from '../lib/Matrix.js';
 
-let controls;
+let circleControls;
+let gridControls;
+let timeControls;
 
 let c, l;
 
@@ -18,13 +21,25 @@ window.setup = function() {
 		2
 	);
 
-	c = new Circle( 'circle1' );
-	l = new Line( 'line1' );
+	// c = new Circle( 'circle1' );
+	// l = new Line( 'line1' );
 
-	controls = Controls.addGroup( 'sketch' );
-	controls.addControl( 'noise', 0, 1, 0, 0.01 );
-	controls.addControl( 'freq', 0, 0.1, 0, 0.001 );
-	controls.addControl( 'time', 0, 10, 0, 0.01 );
+	circleControls = Controls.addGroup( 'circles' );
+	circleControls.addControl( 'radius', 0, 100, 20, 1 );
+	circleControls.addControl( 'start', 0, 1, 0, 0.01 );
+	circleControls.addControl( 'end', 0, 1, 0, 0.01 );
+	circleControls.addControl( 'rotate', 0, 360, 0, 0.01 );
+
+	gridControls = Controls.addGroup( 'grid' );
+	gridControls.addControl( 'width', 0, width, 1, 1 );
+	gridControls.addControl( 'height', 0, height, 1, 1 );
+	gridControls.addControl( 'xres', 0, 100, 1, 1 );
+	gridControls.addControl( 'yres', 0, 100, 1, 1 );
+	gridControls.addControl( 'noise', 0, 100, 0, 0.1 );
+	gridControls.addControl( 'freq', 0, 300, 0, 0.1 );
+	
+	timeControls = Controls.addGroup( 'time' );
+	gridControls.addControl( 'time', 0, 10, 0, 0.01 );
 
 	Snapshots.init( 'Test1' );
 	Snapshots.applyLatest();
@@ -43,37 +58,70 @@ window.draw = function() {
 	background(255, 243, 212); 
 	noFill();
 
-
 	Canvas.pushMatrix();
-	Canvas.translate( -width/2, -height/2 );
+	// Canvas.translate( -width/2, -height/2 );
 
-
-	l.create( 
-		new TransMatrix.Vector3( 0, 0 ),
-		new TransMatrix.Vector3( width, height ),
-		20
+	let grid = new Grid();
+	grid.create( 
+		gridControls.getValue('xres'),
+		gridControls.getValue('yres'),
+		gridControls.getValue('width'),
+		gridControls.getValue('height'),		
+	);	
+	grid.center();
+	grid.noise(
+		gridControls.getValue( 'noise' ),
+		gridControls.getValue( 'freq' ),
+		gridControls.getValue( 'time' )
 	);
-	l.render();
+
+	// fill(0);
+	for( let vert of grid.vertices ) {
+		let myc = new Circle();
+		myc.create(
+			circleControls.getValue('radius'),
+			100,
+			new TransMatrix.Vector3(),
+			circleControls.getValue('start'),
+			circleControls.getValue('end')
+		);
+		myc.rotate( noise( vert.x, vert.y ) * circleControls.getValue('rotate') );
+		myc.translate( vert );
+
+		myc.render();
+	}
+
+
+	
+
+
+	// l.create( 
+	// 	new TransMatrix.Vector3( 0, 0 ),
+	// 	new TransMatrix.Vector3( width, height ),
+	// 	20
+	// );
+	// l.render();
 
 	Canvas.popMatrix();
 
+	// c.create(
+	// 	c.controls.getValue('radius'),		
+	// 	c.controls.getValue('resolution'),
+	// 	new TransMatrix.Vector3(),
+	// 	c.controls.getValue('start'),
+	// 	c.controls.getValue('end')
+	// );
 
-	c.create(
-		c.controls.getValue('radius'),
-		new TransMatrix.Vector3(),
-		c.controls.getValue('resolution'),
-		c.controls.getValue('start'),
-		c.controls.getValue('end')
-	);
+	// c.rotate( c.controls.getValue('rotate'), 'Y' );
 
-	c.noise( 
-		controls.getValue('noise'),
-		controls.getValue('freq'),
-		controls.getValue('time')
-	);
-	c.rotate( c.controls.getValue('rotate') );
+	// c.noise( 
+	// 	controls.getValue('noise'),
+	// 	controls.getValue('freq'),
+	// 	controls.getValue('time')
+	// );
+	
 
-	c.render();
+	// c.render();
 
 	
 
